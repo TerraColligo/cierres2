36ac80f1b22a387636472c6118c9f7974b313b8d
Ossi Mantylahti, TC <ossi.mantylahti@terracolligo.com>
2019-06-28 22:15:48 -0500

    added imp-exp module without dependencies

---
+++ import_product_inventory/__manifest__.py
@@ -0,0 +1,23 @@
+{'name': 'Import product in Batch',
+ 'version': '11.4.29.6.2019',
+ 'category': 'other',
+ 'depends': ['sale_stock','purchase'],
+ 'author': "Terra Colligo",
+ 'license': 'OPL-1',
+ 'website': '',
+ 'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'data/cron.xml',
+        'views/product_import_batch_view.xml',
+        'views/product_cierries_import_batch_view.xml',
+        'wizard/import_product_wizard_view.xml',
+        'wizard/import_cierres_product_wizard_view.xml',
+        'wizard/export_product_view.xml',
+        ],
+  "qweb": [
+       "static/src/xml/base.xml",
+        ],
+ 'installable': True,
+ 'application': True,
+ }
